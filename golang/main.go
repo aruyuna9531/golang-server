3ab1b03fb86029de97232823fe2f1c06b6671cb9
aruyuna9531 <982033268@qq.com>
2021-09-23 02:00:36 +0800

    go-mysql

--- golang/main.go
+++ golang/main.go
@@ -6,6 +6,7 @@ import (
 	"main/myhttp"
 	"main/mytcp"
 	"main/zookeeper"
+	"main/db"
 )
 
 func main() {
@@ -14,6 +15,17 @@ func main() {
 	go mytcp.CreateTcpServer(9000)
 	go myhttp.CreateHttpServer(9001)
 	go zookeeper.LinkZookeeper()
+
+	conf := db.MysqlConf{
+		Username: "root",
+		Password: "mysql",
+		RemoteIp: "192.168.2.181",
+		RemotePort: 3306,
+		DbName: "test_db",
+		OpenConns: 1,
+		IdleConns: 1,
+	}
+	db.LinkMysql(conf)
 
 	for {
 
