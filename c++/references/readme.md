822556474fdf950e1a170daa9a6b3750c14e0429
aruyuna9531 <982033268@qq.com>
2021-10-06 16:22:59 +0800

    remove

---
+++ c++/references/readme.md
@@ -0,0 +1,4 @@
+# 引用
+
+包含了所引用的库。
+clone后使用 git submodule update --init --recursive
--- c++/src/protocol.cpp
+++ c++/src/protocol.cpp
@@ -1,7 +1,11 @@
 #include "protocol.h"
 
-bool putBool(const int key, const bool value);
-bool putInt(const int key, const int value);
-bool putInt64(const int key, const int64_t value);
-bool putString(const int key, const std::string& value);
-bool putProtocol(const int key, const Protocol& value);
+bool Protocol::putBool(const int key, const bool value)
+{
+
+}
+
+bool Protocol::putInt(const int key, const int value);
+bool Protocol::putInt64(const int key, const int64_t value);
+bool Protocol::putString(const int key, const std::string& value);
+bool Protocol::putProtocol(const int key, const Protocol& value);
