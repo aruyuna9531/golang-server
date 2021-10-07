01a11ff323b92321f053dae206cb307be7719109
aruyuna9531 <982033268@qq.com>
2021-10-07 20:27:02 +0800

    整理

--- c++/references/readme.md
+++
@@ -1,4 +0,0 @@
-# 引用
-
-包含了所引用的库。
-clone后使用 git submodule update --init --recursive
--- node.js/main.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-http.createServer((request, response)=>{
-    var args = new URL(request.url, "http://0.0.0.0:8000");
-    // console.log(args);
-    response.write('hello! you are trying to access path: ' + args.pathname + ", your params is: " + args.searchParams);
-}).listen(8000, '0.0.0.0', ()=>{
-    console.log("server started at 0.0.0.0:8000");
-})
